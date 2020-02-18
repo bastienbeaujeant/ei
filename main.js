cf0f582da355da3c68983cfc52db8d1d9ff4174d
bastou <bastien.beaujeant@gmail.com>
2020-02-18 15:43:49 +0100

    More JS and Desktop

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
-let element = document.querySelector(".burger-menu");
+let burgerMenu = document.querySelector(".burger-menu");
 let span = document.querySelectorAll(".burger-menu span");
 let menu = document.querySelector(".header-menu");
 let searchInput = document.querySelector(".search-input");
 let searchIcon = document.querySelector(".search-btn");
-let body = document.querySelector("body")
+let body = document.querySelector("body");
+let slider = document.querySelectorAll(".activity-spot");
 
-element.addEventListener("click", function() {
-  element.classList.toggle("fixed");
+burgerMenu.addEventListener("click", function() {
+  burgerMenu.classList.toggle("fixed");
   menu.classList.toggle("none");
   span.forEach(element => {
     element.classList.toggle("open");
   });
 });
 searchIcon.addEventListener("click", function() {
-    event.preventDefault();
+  event.preventDefault();
   searchInput.classList.toggle("invisible");
 });
 
-element.addEventListener("click", function() {
+burgerMenu.addEventListener("click", function() {
+  event.preventDefault();
+  if (body.classList.contains("noscroll")) {
+    body.classList.remove("noscroll");
+  } else {
+    body.classList.add("noscroll");
+  }
+});
+
+slider.forEach(element => {
+  element.addEventListener("click", function() {
     event.preventDefault();
-    if(body.classList.contains("noscroll")){
-        body.classList.remove("noscroll");
-    }
-    else{
-        body.classList.add("noscroll");
-    }
-    
+    element.classList.toggle("min");
+  });
 });
